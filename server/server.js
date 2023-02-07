8d230ab970690f61cbc2f8be6c2f8a4778ad19b4
Manuvet <manu.vettori02@gmail.com>
2023-02-07 16:09:33 +0100

    Modifica server 2

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(
 );
 
 const port = process.env.PORT || 8080;
-app.locals.db = mongoose.connect(process.env.PORT, { useNewUrlParser: true, useUnifiedTopology: true })
+app.locals.db = mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
 
         console.log("Connected to Database");
