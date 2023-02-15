576956c992a081b7aaf9ceac650251a6613fa075
Manuvet <manu.vettori02@gmail.com>
2023-02-15 15:44:19 +0100

    Logo AllAboutTrento

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,26 @@
 </script>
 
 <template>
-  <main>
-    <h1>Home</h1>
-    AllAboutTrento - University of Trento, 2022/2023
+  <main style='text-align: center;'>
+    <h1 style="font-weight: bold;font-family:verdana;">AllAboutTrento</h1>
+    <img src="../assets/Logo.png" width="800">
+  
+      <div class="center">
+        Il sito ha come obiettivo quello di favorire
+     il turismo culturale di una città d’arte di 
+     medie dimensioni come Trento, <br> offrendo ai 
+     visitatori un servizio per la fruizione di 
+     contenuti multimediali che descrivono i 
+     “punti di interesse” (POI) di tipo monumentale,
+      artistico e culinario.
+      </div>
   </main>
 </template>
+
+<style>
+.center{
+  margin: auto;
+  width: 50%;
+  font-family: verdana;
+}
+</style>
